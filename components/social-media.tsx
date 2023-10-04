d7cf7791761f07db8bdd7a5d4ac228341f8bf4c8
Mauro <mauro.guerrero@glorifi.com>
2023-10-04 11:39:49 -0400

    add social link

--- components/social-media.tsx
+++ components/social-media.tsx
@@ -1,17 +1,21 @@
 "use client"
 
-import { BiLogoDribbble, BiLogoGithub, BiLogoLinkedin, BiLogoTwitter } from "react-icons/bi";
+import { BiLogoDribbble, BiLogoGithub, BiLogoLinkedin, BiLogoTwitter, BiLogoCodepen } from "react-icons/bi";
 import {motion} from 'framer-motion'
 
 
 
-export default function Socials(){ 
+export default function Socials(){
 
 return(
 
-<div className="hidden xl:flex flex-row gap-8 text-gray-500 text-3xl">
+<div className="hidden xl:flex flex-row gap-6 text-gray-500 text-3xl">
 <motion.a href="https://github.com/uxmauro" className='social-icons' target="_blank" whileHover={{y: -6}} whileTap={{scale: 0.95}} >
 <BiLogoGithub/>
+</motion.a>
+
+<motion.a href="https://codepen.com/uxmauro" className='social-icons' target="_blank" whileHover={{y: -6}} whileTap={{scale: 0.95}} >
+<BiLogoCodepen />
 </motion.a>
 
 <motion.a href="https://linkedin.com/in/uxmauro" className='social-icons' target="_blank" whileHover={{y: -6}} whileTap={{scale: 0.95}}>
@@ -22,9 +26,11 @@ return(
 <BiLogoDribbble/>
 </motion.a>
 
+
 <motion.a href="https://twitter.com/uxmauroo" className='social-icons' target="_blank" whileHover={{y: -6}} whileTap={{scale: 0.95}} >
 <BiLogoTwitter />
 </motion.a>
+
 
 </div>
 )
